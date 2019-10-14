bbb358b36a234910f3f773f1bee97585953a42f7
vickyvaibhav <40216299+vickyvaibhav@users.noreply.github.com>
2019-10-14 09:44:26 +0530

    It is mulitple dropdown checkbox
    It is mulitple dropdown checkbox created by vaibhav agrawal.
    I have made use of built in libraries of CSS and JS.

---
+++ js/Fruits.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+	$('#fruits').multiselect({
+		includeSelectAllOption: true,
+		enableFiltering: true,
+		enableCaseInsensitiveFiltering: true,
+		nonSelectedText: 'Select Fruits',
+	
+		onChange: function(option, checked) {
+	        	// alert('Changed option ' + $(option).val() + '.');
+
+	      		 var selectedOptions = $('.demo option:selected');
+ 
+  
+			 var maxLength = 4;
+                	 if (selectedOptions.length >= maxLength) {
+   
+
+		                 // Disable all other checkboxes.
+      
+              			var nonSelectedOptions = $('.demo option').filter(function() {
+    
+                    		return !$(this).is(':selected');
+    
+                		});
+ 
+       
+             			nonSelectedOptions.each(function() {
+    
+                		var input = $('input[value="' + $(this).val() + '"]');
+    
+                    		input.prop('disabled', true);
+    
+                  		     
+               			});
+       
+         		}
+      
+       		   else {
+            
+        			// Enable all checkboxes.
+         
+          			 $('.demo option').each(function() {
+            
+            			var input = $('input[value="' + $(this).val() + '"]');
+            
+            			input.prop('disabled', false);
+               
+     				       
+            			});
+            
+    			}
+
+		}
+	});
+});
